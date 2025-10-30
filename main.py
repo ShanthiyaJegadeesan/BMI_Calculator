47e789d89c6a0df759301626eb8fefaa6ec92c33
ShanthiyaJegadeesan <shanthiyasha2005@gmail.com>
2025-10-30 09:14:40 +0530

    bmi calculation and error display

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import tkinter as tk
+from tkinter import messagebox
+def bmi():
+    try:
+
+        weight=float(weight_entry.get())
+        height=float(height_entry.get())
+        if height<=0 or weight<=0:
+            messagebox.showerror("Invalid Input","Height and Weight must be greater than 0")
+    except ValueError:
+        messagebox.showerror("Input Error","Please input valid number for height and weight")
+
+
+    bmi_calculation= weight/(height**2)
+    print(f"{bmi_calculation:.2f}")
+    
 
 window=tk.Tk()
 window.title("BMI Calculator")
@@ -12,25 +27,25 @@ grid_frame=tk.Frame(
 
 weight_label=tk.Label(
     grid_frame,
-    text="Weight",
+    text="Weight(kg)",
     bg="light yellow",
     font=("Derby",15)
     )
-#weight_label.pack(padx=10,pady=10)
+
 
 weight_entry=tk.Entry(grid_frame,width=15)
-#weight_entry.pack(padx=10)
+
 
 height_label=tk.Label(
     grid_frame,
-    text="Height",
+    text="Height(m)",
     bg="light yellow",
     font=("Derby",15)
     )
-#height_label.pack(padx=10,pady=10)
+
 
 height_entry=tk.Entry(grid_frame,width=15)
-#height_entry.pack(padx=10,pady=10)
+
 
 grid_frame.pack(pady=10)
 weight_label.grid(row=0,column=0,padx=10,pady=5)
@@ -42,12 +57,21 @@ calculate_button=tk.Button(
     window,
     text="Calculate",
     bg="red",
-    font=("derby",15)
+    font=("derby",15),
+    command=bmi
     )
-calculate_button.pack(padx=50)
+calculate_button.pack(padx=5)
 
 calculate_entry=tk.Entry()
 calculate_entry.pack(padx=5,pady=5)
+
+
+
+
+
+    
+
+
 
 
 
